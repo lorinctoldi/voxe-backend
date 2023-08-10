7dd82a4242e584db4e4af854b108530460002275
lorinc-toldi <lorinc.toldi@gmail.com>
2023-08-10 11:50:45 +0200

    yearly filter added

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
 const fs = require('fs');
-const { performance } = require('perf_hooks');
 
-// results max 600 ?
-axios
-  .post('https://www.hasznaltauto.hu/egyszeru/szemelyauto', {
-    marka_id: 12,
-    modell_id: 120,
-    results: 600
-  })
-  .then(res => {
-		const $ = cheerio.load(res.data);
+const baseURL = 'https://www.hasznaltauto.hu/egyszeru/szemelyauto';
+
+async function scrapeDataForYearRange(evjarat_min, evjarat_max) {
+  console.log(evjarat_min, evjarat_max)
+  try {
+    const response = await axios.post(baseURL, {
+      marka_id: 12,
+      modell_id: 102,
+      results: 600,
+      evjarat_min: evjarat_min,
+      evjarat_max: evjarat_max
+    });
+
+    const $ = cheerio.load(response.data);
     const listings = [];
 
     $('div.talalati-sor').each((index, element) => {
-      const title = $(element).find('h3 a').text().replace(/\s/g, '')
-			const price = $(element).find('div.pricefield-primary').first().text().replace('Ft', '').replace(/\s/g, '')
+      const title = $(element).find('h3 a').text();
+      const price = $(element).find('div.pricefield-primary').first().text().replace('Ft', '').replace(/\s/g, '');
       const fuel = $(element).find('span.info').eq(0).text().replace(',','').toLowerCase().replace(/\s/g, '')
       const date = $(element).find('span.info').eq(1).text().replace(',','').replace(/\s/g, '')
       const displacement = $(element).find('span.info').eq(2).text().replace(' cm³,','').replace(/\s/g, '')
       const power_output = $(element).find('span.info').eq(3).text().replace(' kW,','').replace(/\s/g, '')
       const horse_power = $(element).find('span.info').eq(4).text().replace(' LE,', '').replace(/\s/g, '')
       const odometer = $(element).find('span.info').eq(5).text().replace(' km', '').replace(/\s/g, '')
-			listings.push({
+
+      listings.push({
         title: title, 
         price: price, 
         fuel: fuel, 
@@ -32,12 +37,30 @@ axios
         power_output: power_output,
         horse_power: horse_power,
         odometer: odometer
-      })
+      });
     });
 
-		fs.writeFileSync('data.json', JSON.stringify(listings, null, 2))
-		console.log('no error, its done')
-  })
-  .catch((error) => {
-    console.log(error.response.data.error);
-  });
+    return listings;
+  } catch (error) {
+    console.error('Error scraping data:', error);
+    return [];
+  }
+}
+
+const startYear = 1900;
+const endYear = 2023;
+
+async function scrapeAllData() {
+  const allListings = [];
+
+  for (let year = startYear; year <= endYear; year++) {
+    const listings = await scrapeDataForYearRange(year, year + 1);
+    allListings.push(...listings);
+  }
+
+  fs.writeFileSync('data.json', JSON.stringify(allListings, null, 2));
+  console.log(`The list of all scraped data: ${allListings.length}`)
+  console.log('Scraping and writing done.');
+}
+
+scrapeAllData();
