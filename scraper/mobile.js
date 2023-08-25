d3042a43dc89f9c9a254855f67e01b86e2287a7b
lorinc-toldi <lorinc.toldi@gmail.com>
2023-08-25 21:45:56 +0200

    final

--- scraper/mobile.js
+++ scraper/mobile.js
@@ -5,15 +5,16 @@ const filterSuspicious = require('../helper/filter')
 const fuel = require('../helper/getFuelType')
 const conversion = require('../helper/getConversionRate')
 
+const headers = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+};
+
 async function scrapeDataByPage(make, model, startYear, endYear, pageNum) {
   console.log('Currently scraping cars from page number ', pageNum)
   const conversionRate = await conversion.getConversionRate();
   const array = [];
   const url = `https://mobile.de/consumer/api/search/srp/items?page=${pageNum}&url=%2Fauto%2Fsearch.html%3Fdam%3D0%26${(startYear || endYear) ? `fr%3D${(startYear || 1900)}%253A${(endYear || 2023)}%26` : ''}ms%3D${make || 1900}%253B${model || 22}%253B%253B%26od%3Dup%26s%3DCar%26sb%3Dp%26vc%3DCar%26ref%3DsrpHead`;
-  const res = await axios.get(url, 
-    { headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-    }});
+  const res = await axios.get(url, headers);
 
   res.data.items.filter(item => item.title).forEach(item => {
     let attributes = item.attributes
