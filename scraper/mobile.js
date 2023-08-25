72d2927d97b6a8e9f69d9a561610bf4e4f1a2da2
lorinc-toldi <lorinc.toldi@gmail.com>
2023-08-25 21:58:43 +0200

    final

--- scraper/mobile.js
+++ scraper/mobile.js
@@ -6,23 +6,13 @@ const fuel = require('../helper/getFuelType')
 const conversion = require('../helper/getConversionRate')
 
 const headers = {
-  'Authority': 'www.mobile.de',
-  'Method': 'GET',
-  'Path': '/consumer/api/search/srp/items?page=1&url=%2Fauto%2Fsearch.html%3Fdam%3D0%26fr%3D2021%253A2023%26ms%3D22%253B27227%253B%253B%26od%3Dup%26s%3DCar%26sb%',
-  'Scheme': 'https',
-  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-  'Accept-Encoding': 'gzip, deflate, br',
-  'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8,hu;q=0.7',
+  'Pragma': 'no-cache',
   'Cache-Control': 'no-cache',
-  'Sec-Ch-Ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
-  'Sec-Ch-Ua-Mobile': '?1',
-  'Sec-Ch-Ua-Platform': '"Android"',
-  'Sec-Fetch-Dest': 'document',
-  'Sec-Fetch-Mode': 'navigate',
-  'Sec-Fetch-Site': 'none',
-  'Sec-Fetch-User': '?1',
   'Upgrade-Insecure-Requests': '1',
-  'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36',
+  'User-Agent': 'Mozilla/6.1 (Macintosh; ARM Mac OS X 12_12_3) AppleWebKit/737.36 (KHTML, like Gecko) Chrome/62.0.2821.0 Safari/737.36',
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+  'Accept-Encoding': 'gzip, deflate, sdch',
+  'Accept-Language': 'de;q=0.8,en;q=0.6',
 };
 
 async function scrapeDataByPage(make, model, startYear, endYear, pageNum) {
